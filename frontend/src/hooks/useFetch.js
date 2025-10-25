c1dd74fdadbe408ab9890ee3367e03a998204c5d
MHMD304 <slymanmhamad86@gmail.com>
2025-10-25 12:36:45 +0300

    Add useFetch custom hook

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+// useFetch.js
+import { useState, useEffect } from "react";
+
+export const useFetch = (fetchFunction, deps = []) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchFunction()
+      .then((res) => setData(res))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, deps);
+
+  return { data, loading, error };
+};
